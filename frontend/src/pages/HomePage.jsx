62eba5b0056d8502c8b4d701ed6e3cdb8a251673
parasmer <138353860+parasmer@users.noreply.github.com>
2025-06-26 19:52:41 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -28,7 +28,7 @@ const HomePage = () => {
     <>
       <section id="home" className="w-full bg-gray-100  flex items-center justify-center relative py-28 sm:py-36 md:py-44"
          style={{
-           height:100vh,
+           height:auto,
           backgroundImage: `url(${bgimg})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
