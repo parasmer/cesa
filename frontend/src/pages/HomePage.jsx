d854f285782d3d59521214bdf7732cdc4b11c640
parasmer <138353860+parasmer@users.noreply.github.com>
2025-09-01 22:21:02 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -64,11 +64,11 @@ const HomePage = () => {
         </div>
 
         {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+{/*         <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
           <div className="w-6 h-10 border-2 border-blue-300/50 rounded-full flex justify-center">
             <div className="w-1 h-3 bg-blue-300 rounded-full mt-2 animate-pulse"></div>
           </div>
-        </div>
+        </div> */}
       </section>
 
       {/* Enhanced About Us Section */}
